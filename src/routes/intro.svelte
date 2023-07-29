01a4fe9193dc6d3b5e3df1f94952f4e9e11be992
oborekk <oskarb623@gmail.com>
2023-07-29 09:06:04 +0200

    responsive 3

--- src/routes/intro.svelte
+++ src/routes/intro.svelte
@@ -1,8 +1,8 @@
-<div class="d-flex p-5 align-items-center wrapper" style="height: 30vw;">
+<div class="d-flex p-5 align-items-center wrapper">
     <div class="card ms-5 my-3 me">
         <div class="row">
             <div class="col-sm-3">
-              <img src="yo2.jpg" style="max-height: 280px;" class="card-img rounded-start" alt="me">
+              <img src="yo2.jpg" class="card-img rounded-start" alt="me">
             </div>
             <div class="col-sm-9">
               <div class="card-body">
@@ -28,12 +28,12 @@
 .wrapper {
   animation: scroll 500s linear infinite;
   background: url("/sand.avif");
+  min-height: 25vw;
 }
 
 .card-img {
   width: 100%;
-  height: 12vw;
-  object-fit: cover;
+  object-fit: contain;
 }
 
 @keyframes scroll {
@@ -44,7 +44,6 @@
 
 .me {
   border: 0px;
-  max-width: 45vw;
   width: 50vw;
 }
 </style>
